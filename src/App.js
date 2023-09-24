bd77aabcc1b436bfd0ccd257642f7db6df8d1373
Ayansaxena24 <ayan.saxena24@gmail.com>
2023-09-25 00:20:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <p>This is a Website for Challenge 23 of Scaler Open Source Challenge.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
